a26c8ac3844a8fca599435c1a1f16ecc76783d4a
Helder Almeida <hcalmeida@uesc.br>
2021-12-05 22:51:32 -0300

    Projeto finalizado

--- StandAloneVersion/PSOR_helder.h
+++ StandAloneVersion/PSOR_helder.h
@@ -1,60 +1,93 @@
+/***********************************************************************
+    Método SOR para cálculo de matriz simétrica e pentadiagonal
+
+    Projeto desenvolvido por Hélder Almeida - hcalmeida@uesc.br
+    Para fins de crédido no módulo de Técnicas Computacionais
+    lecionado pelo prof. Grazione de Souza
+    parte da disciplina de Fundamentos de Modelagem Computacional
+
+    Período: 2021.2
+    ARQUIVO PSOR_helder.c - biblioteca com as funções implementadas
+***********************************************************************/
+
+#ifndef PSOR_HELDER
+#define PSOR_HELDER
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //casas decimais dos arquivos de saída
-#define DC 6
+#define DC 8
+//máximo de interações até o algoritmo interromper a execução
+#define MAX_INTERATIONS 100
 
-/*
-    Estrutura para armazenar os dados de entrada
-*/
+/**
+    Estrutura DataIn para armazenar os dados de entrada
+**/
 typedef struct{
-    double w, LX, LY, DT, G, f, m, Err;
-    int n, NX, NY;
+    double  w,      ///parâmetro de relaxação
+            LX,     ///utilizado para calcular a diagonal b
+            LY,     ///utilizado para calcular a diagonal c
+            DT,     ///utilizado para calcular a diagonal a
+            G,      ///utilizado para calcular a diagonal a
+            f,      ///utilizado para calcular o vetor d
+            m,      ///utilizado para calcular o vetor d
+            Err;    ///erro absoluto aceitável (tolerância)
+    int     n,      ///ordem da matriz
+            NX,     ///igual a n, utilizado para calcular a diagonal b
+            NY;     ///igual a n, utilizado para calcular a diagonal c
 } DataIn;
 
-/*
-    Estrutura para armazenar os dados de saída
-*/
+/**
+    Estrutura DataOut para armazenar os dados de saída
+**/
 typedef struct{
-    double absErr;
-    int interations;
-    double* xk; //vetor x(k) com chute inicial
-    double* xkm1; //vetor x(k+1) com cada aproximação
+    int tam;        ///dimensão dos vetores x
+    int n;          ///onde tam = n*n
+    double absErr;  ///erro absoluto calculado
+    int interations;///quantidade máxima de interações antes de abortar
+    double* xk;     ///vetor x(k-1) guardando a última interação
+    double* xkm;    ///vetor x(k) com interação em processamento
 } DataOut;
 
-/*
-    Estrutura para armazenar os valores de a, b e c
-*/
+/**
+    Estrutura DataArrays para armazenar os vetores a, b, c e d
+**/
 
 typedef struct{
-    double* a; //vetor da diagonal principal
-    double* b; //vetor da diagonal b
-    double* c; //vetor da diagonal c
-    double* d; //vetor de resultados d
+    double* a; ///vetor da diagonal principal
+    double* b; ///vetor da diagonal b
+    double* c; ///vetor da diagonal c
+    double* d; ///vetor de resultados d
 } DataArrays;
 
-/*
-    Função para pegar os dados de entrada
-    de um arquivo, um por linha, na ordem:
-    n           - inteiro
-    w(omega)    - double
-    LX          - couble
-    LY          - couble
-    DT          - couble
-    G           - couble
-    f           - couble
-    m           - couble
-    Err(erro)    - couble
+/**
+    Parâmetro: string - nome do arquivo de entrada
 
-*/
+    Retorno: DataIn - estrutura preenchida
+
+    Objetivo:Função para pegar os dados de entrada
+    de um arquivo, um por linha, na ordem:
+
+        n           - inteiro
+        w(omega)    - double
+        LX          - double
+        LY          - double
+        DT          - double
+        G           - double
+        f           - double
+        m           - double
+        Err(erro)   - double
+
+**/
 DataIn getDataIn(char* filename){
     DataIn di;
     FILE *fin;
-    char *aux;
 
     if((fin=fopen(filename,"r"))==NULL){
-        printf("Ero na abertura do arquivo %s",filename);
+        printf("Erro na abertura do arquivo %s",filename);
         exit(-1);
     }
 
@@ -73,49 +106,129 @@ DataIn getDataIn(char* filename){
     di.NX = di.n;
     di.NY = di.n;
 
-    printf("Dados de entrada:\n");
-    printf("NX=NY=n= %d\n", di.n);
-    printf("w= %*lf\n", DC, di.w);
-    printf("LX= %*lf\n", DC, di.LX);
-    printf("LY= %*lf\n", DC, di.LY);
-    printf("DT= %*lf\n", DC, di.DT);
-    printf("G= %*lf\n", DC, di.G);
-    printf("f= %*lf\n", DC, di.f);
-    printf("m= %*lf\n", DC, di.m);
-    printf("Erro= %*lf", DC, di.Err);
+    if(validateDataIn(di)){
+
+    }else{
+        printf("\n\nEXECUÇÃO INTERROMPIDA!\nCORRIJA O ");
+        printf("ARQUIVO '%s' E TENTE NOVAMENTE!\n", filename);
+        getchar();
+        exit(-1);
+    }
 
 
     return di;
 }
 
 
-/*
-    Função para salvar os dados de saída em um arquivo
-*/
+/**
+    Parâmetro: DataIn - dados de entrada
 
-void saveDataOut(char* filename, DataOut dout, int tam){
-    FILE* fout;
-    int i;
+    Retorno: verdadeiro - caso os dados estejam corretos
+             falso - caso os dados de entrada estejam com problema
 
-    if((fout=fopen(filename,"w"))==NULL){
-        printf("Ero na criação do arquivo %s",filename);
-        exit(-1);
+    Objetivo: Função para validar os dados de entrada segundo
+              especificação do trabalho
+**/
+int validateDataIn(DataIn di){
+    int erro = 0;
+
+    printf("\nDados de entrada:\n");
+
+
+
+    printf("NX= %d", di.NX);
+    if( di.NX < 3 || di.NX % 2 != 1 ){
+        printf(" -> Erro: NX deve ser inteiro, ímpar e >= 3\n");
+        erro = 1;
+    }else{
+        printf(" -> Ok\n");
     }
 
-    fprintf(fout,"\nErro alcançado: %10.*lf\n",DC, dout.absErr);
-    fprintf(fout,"Interações: %10d\n", dout.interations);
-    fprintf(fout,"\nVetor X:\n");
-    for(i=0; i<tam; i++){
-        fprintf(fout,"x(%2d)=%10.*lf\n",i,DC,dout.xkm1[i]);
+    printf("NY= %d", di.NY);
+    if( di.NY < 3 || di.NY % 2 != 1 ){
+        printf(" -> Erro: NY deve ser inteiro, ímpar e >= 3\n");
+        erro = 1;
+    }else{
+        printf(" -> Ok\n");
     }
-    fprintf(fout,"------------------------------");
-    fclose(fout);
+
+    printf("w= %*lf", DC, di.w);
+    if( di.w < 1 || di.w > 2 ){
+        printf(" -> Erro: w deve estar entre 1 e 2\n");
+        erro = 1;
+    }else{
+        printf(" -> Ok\n");
+    }
+
+    printf("LX= %*lf", DC, di.LX);
+    if( di.LX <= 0 ){
+        printf(" -> Erro: LX deve ser positivo\n");
+        erro = 1;
+    }else{
+        printf(" -> Ok\n");
+    }
+
+    printf("LY= %*lf", DC, di.LY);
+    if( di.LY <= 0 ){
+        printf(" -> Erro: LY deve ser positivo\n");
+        erro = 1;
+    }else{
+        printf(" -> Ok\n");
+    }
+
+    printf("DT= %*lf", DC, di.DT);
+    if( di.DT <= 0 ){
+        printf(" -> Erro: DT deve ser positivo\n");
+        erro = 1;
+    }else{
+        printf(" -> Ok\n");
+    }
+
+    printf("G= %*lf", DC, di.G);
+    if( di.G <= 0 ){
+        printf(" -> Erro: G deve ser positivo\n");
+        erro = 1;
+    }else{
+        printf(" -> Ok\n");
+    }
+
+    printf("f= %*lf", DC, di.f);
+    if( di.f <= 0 ){
+        printf(" -> Erro: f deve ser positivo\n");
+        erro = 1;
+    }else{
+        printf(" -> Ok\n");
+    }
+
+    printf("m= %*lf", DC, di.m);
+    if( di.m <= 0 ){
+        printf(" -> Erro: m deve ser positivo\n");
+        erro = 1;
+    }else{
+        printf(" -> Ok\n");
+    }
+
+    printf("Erro= %*lf", DC, di.Err);
+    if( di.Err <= 0 ){
+        printf(" -> Erro: erro deve ser positivo\n");
+        erro = 1;
+    }else{
+        printf(" -> Ok\n");
+    }
+
+
+    return (!erro);
 
 }
 
-/*
-*   Calcula as diagonais
-*/
+/**
+    Parâmetro:  DataIn - dados de entrada
+
+    Retorno: DataArrays - vetores com diagonais (a, b e c)
+                          e o vetor respostas d
+
+    Objetivo: Calcula as diagonais de acordo com a especificação
+**/
 DataArrays initDiagonals(DataIn in){
     DataArrays da;
     int i, j, tam;
@@ -177,82 +290,145 @@ DataArrays initDiagonals(DataIn in){
     return da;
 }
 
-/*
-    Função para calcular o erro absoluto
-*/
+/**
+    Parâmetro:  DataOut* - referência para osdados de saída
 
-double absoluteError(double* xk, double* xk1, int tam){
+    Retorno: double - vetores com diagonais (a, b e c)
+                      e o vetor respostas d
+
+    Objetivo: Função para calcular o erro absoluto do vetor xk
+**/
+
+double absoluteError(DataOut *out){
     int i;
     double maxErr, err;
 
-    maxErr = fabs(xk1[0]-xk[0]);
+    maxErr = fabs(out->xkm[0]-out->xk[0]);
 
-    for(i=1; i<tam; i++){
-        err = fabs(xk1[i]-xk[i]);
+    for(i=1; i<out->tam; i++){
+        err = fabs(out->xkm[i]-out->xk[i]);
 
         if(err > maxErr)
             maxErr = err;
     }
 
+    out->absErr = maxErr;
+
     return maxErr;
 }
 
 
-/*
-    Algoritmo SOR Modificado para o escopo do projeto
-*/
-DataOut SORModif(DataArrays da, DataIn in, int maxInterations){
-    int i, tam;
+/**
+    Parâmetro:  DataArrays - vetores com diagonais e vetor d
+                DataIn - dados de entrada
+
+    Retorno: DataOut - vetor xk calculado pelo método SOR
+                       e demais dados dos resultados de saída,
+                       como número de interações e erro alcançado
+
+    Objetivo: Algoritmo SOR Modificado para o escopo do projeto
+
+**/
+DataOut SORModif(DataArrays da, DataIn in){
+    int i;
     DataOut out;
     double xk;
 
-    tam = in.n*in.n;
+    out.n = in.n;
+    out.tam = in.n*in.n;
 
     //alocando memória para o vetor x(k) e iniciando com 0
-    out.xk = calloc(tam, sizeof(double));
+    out.xk = calloc(out.tam, sizeof(double));
 
     //alocando memória para o vetor x(k+1) e iniciando com 0
-    out.xkm1 = calloc(tam, sizeof(double));
+    out.xkm = calloc(out.tam, sizeof(double));
 
 
-    out.interations = 1;
-    while(out.interations++ < maxInterations){
+    out.interations = 0;
+    while(out.interations < MAX_INTERATIONS){
 
         // salvando os valores de x(k+1) em x(k) para cálculo do erro da rodada
-        for(i=0; i<tam; i++)
-            out.xk[i] = out.xkm1[i];
+        for(i=0; i<out.tam; i++)
+            out.xk[i] = out.xkm[i];
 
-        //cálculo da primeira linha - x[0](k+1)
-        out.xkm1[0] = ((1-in.w)*out.xkm1[0])+(in.w/da.a[0])*(da.d[0] - (da.b[0]*out.xk[1] + da.c[0]*out.xk[in.NY]));
+        //cálculo da primeira linha da matriz - x[0](k+1)
+        out.xkm[0] = ((1-in.w)*out.xk[0])+(in.w/da.a[0])*(da.d[0] - (da.b[0]*out.xk[1] + da.c[0]*out.xk[in.NY]));
 
         //cálculo da segunda até (NX-1)-ésima linha - x[1](k+1) : x[NX-1](k+1)
         for(i=1; i<in.n; i++){
-            out.xkm1[i] = ((1-in.w)*out.xkm1[i])+(in.w/da.a[i])*(da.d[i] - da.b[i-1]*out.xkm1[i-1] - (da.b[i]*out.xk[i+1] + da.c[i]*out.xk[in.NY+i]));
+            out.xkm[i] = ((1-in.w)*out.xk[i])+(in.w/da.a[i])*(da.d[i] - da.b[i-1]*out.xkm[i-1] - (da.b[i]*out.xk[i+1] + da.c[i]*out.xk[in.NY+i]));
         }
 
         //cálculo da (NX)-ésima até (NX*NY-NX-1)-ésima linha - x[NX](k+1) : x[NX*NY-NX-1](k+1)
-        for(; i<tam-in.n; i++)
-            out.xkm1[i] = ((1-in.w)*out.xkm1[i])+(in.w/da.a[i])*(da.d[i] - (da.c[i-in.NY]*out.xkm1[i-in.NY]+da.b[i-1]*out.xkm1[i-1]) - (da.b[i]*out.xk[i+1]+da.c[i]*out.xk[in.NY+i]));
+        for(; i<out.tam-in.n; i++)
+            out.xkm[i] = ((1-in.w)*out.xk[i])+(in.w/da.a[i])*(da.d[i] - (da.c[i-in.NY]*out.xkm[i-in.NY]+da.b[i-1]*out.xkm[i-1]) - (da.b[i]*out.xk[i+1]+da.c[i]*out.xk[in.NY+i]));
 
         //cálculo da (NX*NY-NX)-ésima até (NX*NY-2)-ésima linha - x[NX*NY-NX](k+1) : x[NX*NY-2](k+1)
-        for(; i<tam-1; i++)
-            out.xkm1[i] = ((1-in.w)*out.xkm1[i])+(in.w/da.a[i])*(da.d[i] - (da.c[i-in.NY]*out.xkm1[i-in.NY]+da.b[i-1]*out.xkm1[i-1]) - da.b[i]*out.xk[i+1] );
+        for(; i<out.tam-1; i++)
+            out.xkm[i] = ((1-in.w)*out.xk[i])+(in.w/da.a[i])*(da.d[i] - (da.c[i-in.NY]*out.xkm[i-in.NY]+da.b[i-1]*out.xkm[i-1]) - da.b[i]*out.xk[i+1] );
 
         //cálculo da última linha - x[NX*NY-1](k+1)
-        out.xkm1[i] = ((1-in.w)*out.xkm1[i])+(in.w/da.a[i])*(da.d[i] - (da.c[i-in.NY]*out.xkm1[i-in.NY]+da.b[i-1]*out.xkm1[i-1])) ;
+        out.xkm[i] = ((1-in.w)*out.xk[i])+(in.w/da.a[i])*(da.d[i] - (da.c[i-in.NY]*out.xkm[i-in.NY]+da.b[i-1]*out.xkm[i-1])) ;
 
+        out.interations++;
         //calculando o erro e absoluto e saindo do laço caso tenha atingido
-        out.absErr = absoluteError(out.xk, out.xkm1, tam);
+        absoluteError(&out);
         if(out.absErr <= in.Err) break;
 
     }
 
     return out;
 }
-/*
-*   Para gerar um arquivo CSV com a matriz. Para fins de visualização e teste
-*/
-void generateCSVMatriz(int n, DataArrays data, DataOut out, char * filename, char * separator){
+
+/**
+    Parâmetro:  string - nome do arquivo
+                DataOut - dados para serem salvos no arquivo
+    Retorno: sem retorno
+
+    Objetivo: Função para salvar os dados de saída em um arquivo
+**/
+void saveDataOut(char* filename, DataOut dout){
+    FILE* fout;
+    int i,j;
+
+    if((fout=fopen(filename,"w"))==NULL){
+        printf("Erro na criação do arquivo %s",filename);
+        exit(-1);
+    }
+
+    fprintf(fout,"\nErro alcançado: %10.*lf\n",DC, dout.absErr);
+    fprintf(fout,"Interações: %10d\n", dout.interations);
+    fprintf(fout,"\nVetor X:\n");
+    for(i=0; i<dout.tam; i++){
+        fprintf(fout,"x[%03d] = %15.*lf\n",i,DC,dout.xkm[i]);
+    }
+    fprintf(fout,"------------------------------");
+
+    fprintf(fout,"\nVetor X em matriz:\n");
+
+    for(i=0; i<dout.n; i++){
+        for(j=0; j<dout.n; j++)
+            fprintf(fout,"%15.*lf",DC,dout.xkm[i*dout.n+j]);
+        fprintf(fout,"\n");
+    }
+    fclose(fout);
+
+}
+
+
+/**
+    Parâmetro:  DataArrays - vetores com diagonais (a, b e c) e vetor d
+                DataOut - dados de saída calculados pelo SOR
+                int - ordem da matriz a ser impressa
+                string - nome do arquivo csv
+                string - separados de colunas do csv
+
+    Retorno: sem retorno
+
+    Objetivo: Gerar um arquivo CSV com a matriz.
+              Para fins de visualização e testes.
+**/
+void generateCSVMatriz(DataArrays data, DataOut out, char * filename, char * separator){
     FILE *fp;
     int i,j;
 
@@ -262,44 +438,45 @@ void generateCSVMatriz(int n, DataArrays data, DataOut out, char * filename, cha
     }
 
     fprintf(fp, "A%s",separator);
-    for(i=0; i<n*n; i++)
+    for(i=0; i<out.tam; i++)
         fprintf(fp, "%d%s",i,separator);
 
     fprintf(fp, "%s%s%s%s%s%s%s%s%s\n"," ",separator,"x",separator,"xk",separator," ",separator,"d");
 
-    for(i=0; i<n*n; i++){
+    for(i=0; i<out.tam; i++){
         fprintf(fp, "%d%s",i,separator);
-        for(j=0; j<n*n; j++){
+        for(j=0; j<out.tam; j++){
             if(i==j)
                 fprintf(fp, "%.*lf%s", DC, data.a[i], separator);
             else if(j==i+1)
                 fprintf(fp, "%.*lf%s",DC, data.b[i],separator);
             else if(i==j+1)
                 fprintf(fp, "%.*lf%s",DC, data.b[j],separator);
-            else if(i==j+n)
+            else if(i==j+out.n)
                 fprintf(fp, "%.*lf%s",DC, data.c[j],separator);
-            else if(j==i+n)
+            else if(j==i+out.n)
                 fprintf(fp, "%.*lf%s",DC, data.c[i],separator);
             else
                 fprintf(fp, "%s%s","0",separator);
         }
-        fprintf(fp, "%s%s%.*lf%s%.*lf%s%s%s%.*lf"," ",separator,DC,out.xk[i],separator,DC,out.xkm1[i],separator," ",separator,DC,data.d[i]);
+        fprintf(fp, "%s%s%.*lf%s%.*lf%s%s%s%.*lf"," ",separator,DC,out.xk[i],separator,DC,out.xkm[i],separator," ",separator,DC,data.d[i]);
 
 
         fprintf(fp, "\n");
     }
 
     fprintf(fp, "\n");
-    for(i=0; i<n*n+2; i++)
+    for(i=0; i<out.tam+2; i++)
         fprintf(fp, "%s",separator);
 
     fprintf(fp, "%s%s%d","Interações",separator,out.interations);
 
     fprintf(fp, "\n");
-    for(i=0; i<n*n+2; i++)
+    for(i=0; i<out.tam+2; i++)
         fprintf(fp, "%s",separator);
 
     fprintf(fp, "%s%s%lf","Erro",separator,out.absErr);
 
     fclose(fp);
 }
+#endif // PSOR_HELDER
--- StandAloneVersion/projeto.c
+++ StandAloneVersion/projeto.c
@@ -1,10 +1,14 @@
 /***********************************************************************
-    Projeto desenvolvido por Hélder Almeida
+    Método SOR para cálculo de matriz simétrica e pentadiagonal
+
+    Projeto desenvolvido por Hélder Almeida - hcalmeida@uesc.br
     Para fins de crédido no módulo de Técnicas Computacionais
     lecionado pelo prof. Grazione de Souza
     parte da disciplina de Fundamentos de Modelagem Computacional
 
     Período: 2021.2
+
+    ARQUIVO projeto.c - função main
 ***********************************************************************/
 #include <stdlib.h>
 #include <locale.h>
@@ -13,22 +17,60 @@
 #include "PSOR_helder.h"
 
 int main(void){
-    setlocale(LC_ALL, "Portuguese"); //para usar a , ao invés de . no decimal. Inclusive no arquivo de entrada
+    //para usar a ',' ao invés de '.' no decimal. Inclusive no arquivo de entrada
+    setlocale(LC_ALL, "Portuguese");
     DataIn entrada;
     DataOut saida;
     DataArrays valores_matriz;
+    char filename[255] = "entrada.txt";
 
-    entrada = getDataIn("entrada.txt");
+    printf("---------------------------------------------------------------------\n");
+    printf("    Método SOR para cálculo de matriz simétrica e pentadiagonal.     \n");
+    printf("    Projeto desenvolvido por Hélder Almeida - hcalmeida@uesc.br      \n");
+    printf("    Para fins de crédido no módulo de Técnicas Computacionais        \n");
+    printf("    lecionado pelo prof. Grazione de Souza                           \n");
+    printf("    parte da disciplina de Fundamentos de Modelagem Computacional    \n");
+    printf("    Período: 2021.2                                                  \n");
+    printf("---------------------------------------------------------------------\n");
 
+    printf("\n --- Etapa 1 de 4 (Entrada de dados) --- \n");
+    printf("Informe o nome do arquivo de entrada\n");
+    printf("Tecle enter para aceitar 'entrada.txt': ");
+    gets(filename);
+    if(filename[0]=='\0')
+        strcpy(filename,"entrada.txt");
+
+    entrada = getDataIn(filename);
+
+    printf("\n --- Etapa 2 de 4 (Criação dos vetores) --- \n");
     valores_matriz = initDiagonals(entrada);
 
-    saida = SORModif(valores_matriz,entrada,50);
 
-    saveDataOut("saida.txt",saida,entrada.n*entrada.n);
+    printf("\n --- Etapa 3 de 4 (Calculando vetor x) --- \n");
+    saida = SORModif(valores_matriz,entrada);
 
-    //criando um arquivo CSV (delimitador ';') para visualizar a matriz
-    generateCSVMatriz(entrada.n, valores_matriz, saida, "teste.csv",";");
+    printf("\n --- Etapa 4 de 4 (Salvando a saída) --- \n");
+    printf("Informe o nome do arquivo de saída\n");
+    printf("Tecle enter para aceitar 'saida.txt': ");
+    gets(filename);
+    if(filename[0]=='\0')
+        strcpy(filename,"saida.txt");
 
+    saveDataOut(filename,saida);
+
+    printf("\nDeseja exportar a matriz para um arquivo .csv?\n");
+    printf("(s-sim / n-não): ");
+    gets(filename);
+    if(filename[0]!='n' || filename[0]=='N'){
+        printf("Informe o nome do arquivo csv\n");
+        printf("Tecle enter para aceitar 'saida.csv': ");
+        gets(filename);
+        if(filename[0]=='\0')
+            strcpy(filename,"saida.csv");
+        generateCSVMatriz(valores_matriz, saida, filename,";");
+    }
+
+    printf("\n --- Fim da Execução - OBRIGADO! --- \n");
     getchar();
 
     return 0;
